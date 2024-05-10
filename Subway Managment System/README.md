18fda36b9fe4c88854d86881e74bea13059811cc
Rudra Ponkshe <rudra.ponkshe@gmail.com>
2024-05-10 16:47:13 +0530

    (v1.0) Added first complete version of Subway Management Software

--- Subway Managment System/README.md
+++ Subway Managment System/README.md
@@ -2,35 +2,31 @@
 
 This C++ program simulates a subway management system where you can create stations, lines, and manage their details.
 
-## Features:
+## Features
 
 - Add new stations with name, number of platforms, and accessibility information.
 - Create new subway lines and assign names to them.
-- Add existing stations to existing lines. (Functionality not yet implemented in removeStationFromLine)
+- Add existing stations to existing lines.
+- Remove stations from existing lines.
 - View a list of all stations and their details.
 - View a list of all lines and the number of stations on each line.
 
-## Running the program:
-
-1. Compile the code using a C++ compiler (e.g., g++).
-2. Run the executable file.
-
-## User Interface:
+## User Interface
 
 The program presents a menu-driven interface where you can choose from various operations.
 
-## Global Variables:
+## Global Variables
 
 - `stations_count`: Keeps track of the total number of stations created.
 - `lines_count`: Keeps track of the total number of lines created.
 
-## Classes:
+## Classes
 
 - `Station`: Represents a subway station with properties like name, number of platforms, and accessibility for disabled individuals.
 - `Line`: Represents a subway line with a name and an array to store stations belonging to that line.
 - `subwaySystem`: Not currently used in this implementation, but could be used to manage the overall subway system with multiple lines and stations.
 
-## Functions:
+## Functions
 
 - `getInputString()`: Prompts the user for a string input and returns the entered string.
 - `getInputInt()`: Prompts the user for an integer input and returns the entered value.
@@ -40,8 +36,7 @@ The program presents a menu-driven interface where you can choose from various o
 - `printLineTopics()`: Prints table headers for displaying line details (name, number of stations).
 - `showAllStations()`: Displays a list of all stations with their details.
 - `showAllLines()`: Displays a list of all lines with their names and the number of stations on each line.
-- `addStationToLine()`: Prompts the user to choose a line and a station, and attempts to add the station to the chosen line. (Functionality not yet complete)
-
-## Note:
-
-The `removeStationFromLine` functionality is not yet implemented and requires further development.
+- `addStationToLine()`: Prompts the user to choose a line and a station, and attempts to add the station to the chosen line.
+- `removeStation()`: Prompts the user to choose a station and removes it from the system.
+- `removeLine()`: Prompts the user to choose a line and removes it from the system.
+- `removeStationFromLine()`: Prompts the user to choose a line and a station, and removes the station from the chosen line.
--- Subway Managment System/subway.cpp
+++ Subway Managment System/subway.cpp
@@ -1,36 +1,35 @@
 #include<iostream>
-#include<string.h>
+#include<cstring>
 #include<iomanip>
 #define MAX_SIZE 50
 #define MAX_STATIONS_ON_LINE 20
 #define MAX_LINES_IN_SYSTEM 5
 using namespace std;
+void showAllLines();
 // Global Variables Declared Below:
 int stations_count=0,lines_count=0;
 
 //User defined Functions Start below
+
 string getInputString() {
     string result;
     char inputBuffer[MAX_SIZE];
-    cout<<"Enter the string: ";
     cin.ignore();
     cin.getline(inputBuffer,MAX_SIZE,'\n');
     for(int i=0;inputBuffer[i]!='\0';i++){
         result=result+inputBuffer[i];
     }
-    cout<<result<<endl;
     return result;
 }
 
 int getInputInt(){
     int num;
-    cout<<"Enter an integer: ";
     cin>>num;
     return num;
 }
 bool getInputBool(){
     bool flag;
-    cout<<"Enter 0 for no, and 1 for yes: ";
+    cout<<"1/0(true/false) ";
     cin>>flag;
     return  flag;
 }
@@ -44,7 +43,7 @@ void printStationTopics(){
     cout<<left<<setw(3)<<"No."<<setw(20)<<"Name"<<setw(14)<<"Platforms"<<setw(10)<<"Accessible"<<endl;
     }
 void printLineTopics(){
-    cout<<left<<setw(3)<<"No."<<setw(20)<<"Name"<<setw(14)<<"Platforms"<<endl;
+    cout<<left<<setw(3)<<"No."<<setw(20)<<"Name"<<setw(14)<<"Stations"<<endl;
     }
 class Station{
     string name;
@@ -83,11 +82,26 @@ class Station{
 
 }sub_st[100];
 
-
+void createStation(){
+    string name;
+    int numberOfPlatforms;
+    bool accessible;
+    cout<<"Enter the name of the station: ";
+    name=getInputString();
+    sub_st[stations_count].setName(name);
+    cout<<"Please enter the number of platforms in "<<name<<": ";
+    numberOfPlatforms=getInputInt();
+    sub_st[stations_count].setPlatforms(numberOfPlatforms);
+    cout<<"Is "<<name<<" accessible to disabled users? ";
+    accessible=getInputBool();
+    sub_st[stations_count].setAccessibility(accessible);
+    cout<<"Station created successfully!"<<endl;
+    stations_count++;
+    }
 
 class Line{
     string name;
-    Station stations[MAX_STATIONS_ON_LINE];
+    Station *stations[MAX_STATIONS_ON_LINE];
     int currentNumberOfStations;
     public:
         int numberOfStations(){
@@ -97,10 +111,18 @@ class Line{
             cout<<"Enter the name of the line\n\t";
             name=getInputString();
         }
+        bool checkStation(Station arg){
+            for(int i=0;i<currentNumberOfStations;i++){
+                if(arg.getName()==(stations[i]->getName())){
+                    return true;
+                }
+            }
+            return false;
+        }
 
         void addStation(Station *station){
             if(currentNumberOfStations<MAX_STATIONS_ON_LINE){
-                stations[currentNumberOfStations]=*station;
+                stations[currentNumberOfStations]=station;
                 currentNumberOfStations++;
             }else{
                 cout<<"The line has reached it's limit for stations!"<<endl;
@@ -110,7 +132,7 @@ class Line{
         void removeStation(Station *station){
             int found=0;
             for(int i=0;i<currentNumberOfStations;i++){
-                if(stations[i].getName()==(*station).getName()){
+                if((*stations)[i].getName()==(*station).getName()){
                     found=1;
                     currentNumberOfStations--;
                 }
@@ -118,28 +140,69 @@ class Line{
             }
             cout<<"Please note that the station is only removed from the line, and not from the overall list of stations"<<endl;
         }
+        void removeStation(int index){
+            if(index<=currentNumberOfStations){
+                for(int i=index-1;i<currentNumberOfStations;i++){
+                    stations[i]=stations[i+1];
+                }
+                currentNumberOfStations--;
+            }
+        }
         void showDetails(int no){
             printHeader(37);
             cout<<left<<setw(3)<<no+1<<setw(20)<<name<<setw(14)<<currentNumberOfStations<<endl;
         }
         void showStationsInfo(){
-            printHeader(25);
-            cout<<left<<setw(3)<<"No."<<setw(20)<<"Name"<<endl;
-            printHeader(25);
+            printHeader(47);
+            printStationTopics();
+            printHeader(47);
+            for(int i=0;i<currentNumberOfStations;i++){
+                stations[i]->showDetails(i);
+            }
+            printHeader(47);
 
         }
 
 }sub_lines[5];
 
+void createLine(){
+    if(lines_count>=MAX_LINES_IN_SYSTEM){
+        cout<<"Too many lines in the system!\nRemove lines before adding new ones\n\n"<<endl;
+    }else{
+        sub_lines[lines_count].setName();
+        lines_count++;
+       }
+    }
+
 
 class subwaySystem{
-    Line lines[MAX_LINES_IN_SYSTEM];
+    Line *lines[MAX_LINES_IN_SYSTEM];
     int currentNumberOfLines;
 public:
     int numberOfLines(){
         return currentNumberOfLines;
         }
-};
+    void removeStationFromLine(){
+        int lineCode,stationCode;
+        showAllLines();
+        cout<<"Choose which line you want to remove a station from: ";
+        cin>>lineCode;
+        if(lineCode<=lines_count){
+            sub_lines[lineCode-1].showStationsInfo();
+            cout<<"Choose which station you want to remove: ";
+            cin>>stationCode;
+            if(stationCode<=sub_lines[lineCode-1].numberOfStations()){
+                sub_lines[lineCode-1].removeStation(stationCode);
+            }
+            else{
+                cout<<"Please enter a valid Station Code"<<endl;
+            }
+        }else{
+            cout<<"Please enter a valid line number"<<endl;
+        }
+    }
+
+}subway;
 
 void showAllStations(){
         printHeader();
@@ -162,6 +225,14 @@ void showAllLines(){
         printHeader(37);
     }
 
+void showStationsOnLine(){
+    int lineNum;
+    showAllLines();
+    cout<<"\n\nEnter the Line Number : " ;
+    cin>>lineNum;
+    sub_lines[lineNum-1].showStationsInfo();
+}
+
 void addStationToLine(){
         int chooseLine,chooseStation;
         showAllLines();
@@ -172,63 +243,144 @@ void addStationToLine(){
         cin>>chooseStation;
         sub_lines[chooseLine-1].addStation(&sub_st[chooseStation-1]);
 }
-void removeStationFromLine(){
-    int chooseLine,chooseStation;
-
+void removeStation(){
+    int index;
+    showAllStations();
+    cout<<"Which station do you want to delete?"<<endl;
+    cin>>index;
+    for(int i=0;i<lines_count;i++){
+        if(sub_lines->checkStation(sub_st[i])){
+            sub_lines[i].removeStation(&sub_st[i]);
+            break;
+        }
+    }
+    for(int i=index;i<stations_count;i++){
+        sub_st[index]=sub_st[index+1];
+    }
+    stations_count--;
 }
+
+void removeLine(){
+    int index;
+    showAllLines();
+    cout<<"Which line do you want to remove? ";
+    cin>>index;
+    if((index>MAX_LINES_IN_SYSTEM||index>lines_count)){
+        cout<<"\nNumber of lines in the system are "<<lines_count<<endl;
+        cout<<"Please enter a valid number!"<<endl;
+        return;
+    }
+    for(int i=index-1;i<lines_count;i++){
+        sub_lines[i]=sub_lines[i+1];
+    }
+    lines_count--;
+    cout<<"Line removed successfully"<<endl;
+    }
+
+void noObjectsError(string name){
+    cout<<"Please create a "<<name<<" before using this function!\n\n"<<endl;
+    }
 
 int main(){
     int cho=1;
     Station station[MAX_STATIONS_ON_LINE*MAX_LINES_IN_SYSTEM];
     cout<<right<<setw(50)<<"Subway Management System"<<endl;
     while(cho!=0){
+        cout<<"\n\n"<<endl;
         cout<<"Choose your operation"<<endl;
         cout<<"1. Add a new Station"<<endl;
         cout<<"2. Create a new line"<<endl;
         cout<<"3. Add a station to a line"<<endl;
         cout<<"4. Remove station from a line"<<endl;
-        cout<<"5. Show all stations"<<endl;
+        cout<<"5. Show all stations on line"<<endl;
         cout<<"6. Show all lines"<<endl;
+        cout<<"7. Show all stations"<<endl;
+        cout<<"8. Remove station"<<endl;
+        cout<<"9. Remove line"<<endl;
+        cout<<"0. Exit"<<endl;
         cout<<"Enter your choice: ";
         cin>>cho;
         system("cls");
         switch(cho){
             case 1:{
-                sub_st[stations_count].setName(getInputString());
-                sub_st[stations_count].setPlatforms(getInputInt());
-                sub_st[stations_count].setAccessibility(getInputBool());
-                cout<<"Station created successfully!"<<endl;
-                stations_count++;
+                createStation();
             }
             break;
             case 2:{
-                    if(lines_count>=MAX_LINES_IN_SYSTEM){
-                        cout<<"Too many lines in the system!\nRemove lines before adding new ones"<<endl;
-                    }else{
-                        sub_lines[lines_count].setName();
-                        lines_count++;
-                        }
-                }
+                createLine();
                 break;
 
             case 3:
             {
+                if(stations_count==0||lines_count==0){
+                    noObjectsError("station and a line");
+                    break;
+                }
                 addStationToLine();
             }
             break;
             case 4:
             {
-
+                if(stations_count==0||lines_count==0){
+                    noObjectsError("station and a line");
+                    break;
+                }
+                subway.removeStationFromLine();
             }
+
+            break;
             case 5:{
-                showAllStations();
+                if(lines_count==0){
+                    noObjectsError("line");
+                    break;
+                }
+                showStationsOnLine();
             }
             break;
             case 6:
             {
+                if(lines_count==0){
+                    noObjectsError("line");
+                    break;
+                }
                 showAllLines();
-            }break;
+            }
+
+            break;
+            case 7:
+            {
+                if(stations_count==0){
+                    noObjectsError("station");
+                    break;
+                }
+                showAllStations();
+            }
+            break;
+            case 8:
+            {
+                if(stations_count==0){
+                    noObjectsError("station");
+                    break;
+                }
+                removeStation();
+                break;
+            }
+            case 9:
+            {
+                if(lines_count==0){
+                    noObjectsError("line");
+                    break;
+                }
+                removeLine();
+                break;
+            }
+            case 0:
+            {
+                cout<<"Thanks for using! Have a good day"<<endl;
+            }
         }
     }
+    }
     return 0;
+
 }
